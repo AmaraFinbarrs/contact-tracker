81003aec7f6c26deb80050c5f63670ed2644ec80
Amara Finbarrs-Ezema <amarafinbarrs123@gmail.com>
2024-09-21 05:25:25 +0100

    Create the index page

---
+++ src/routes/index.jsx
@@ -0,0 +1,10 @@
+export default function Index() {
+    return (
+      <p id="zero-state">
+        Welcome to Contact Tracker App.
+        <br />
+        Start by creating a new contact or selecting an existing contact from the list on the left
+        .
+      </p>
+    );
+}
--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -15,7 +15,7 @@ export default function Root() {
     return (
       <>
         <div id="sidebar">
-          <h1>React Router Contacts</h1>
+          <h1>Contact Tracker App</h1>
           <div>
             <form id="search-form" role="search">
               <input
