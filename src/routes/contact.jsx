c44dfcc4f402f5cae197958c3f8f302cba760aee
Amara Finbarrs-Ezema <amarafinbarrs123@gmail.com>
2024-09-23 13:12:59 +0100

    Handle favourite contact with optimistic UI

--- src/routes/contact.jsx
+++ src/routes/contact.jsx
@@ -1,9 +1,20 @@
-import { Form, useLoaderData } from "react-router-dom";
-import { getContact } from "../contacts";
+import { 
+  Form, 
+  useLoaderData,
+  useFetcher,
+} from "react-router-dom";
+import { getContact, updateContact } from "../contacts";
 
 export async function loader({ params }) {
     const contact = await getContact(params.contactId);
     return { contact };
+}
+
+export async function action({ request, params }) {
+  const formData = await request.formData();
+  return updateContact(params.contactId, {
+    favorite: formData.get("favorite") === "true",
+  });
 }
 
 export default function Contact() {
@@ -72,9 +83,13 @@ export default function Contact() {
 }
 
 function Favorite({ contact }) {
-  const favorite = contact.favorite;
+  const fetcher = useFetcher();
+  const favorite = fetcher.formData
+  ? fetcher.formData.get("favourite") === "true"
+  : contact.favorite;
+
   return (
-    <Form method="post">
+    <fetcher.Form method="post">
       <button
         name="favorite"
         value={favorite ? "false" : "true"}
@@ -86,6 +101,6 @@ function Favorite({ contact }) {
       >
         {favorite ? "★" : "☆"}
       </button>
-    </Form>
+    </fetcher.Form>
   );
 }
